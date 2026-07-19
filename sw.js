91d801b7c8b3ea18392196e8a39524e4b42526c1
somlosocram-star <somlosocram@gmail.com>
2026-07-19 13:15:13 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'onix-v0.1.0';
+const ASSETS = [
+  './', './index.html', './manifest.webmanifest',
+  './icon-192.png', './icon-512.png', './favicon-32.png', './apple-touch-icon.png'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks =>
+    Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c => { try { c.put(e.request, copy); } catch (err) {} });
+      return resp;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
