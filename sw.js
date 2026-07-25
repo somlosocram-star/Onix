bae5bda7420c663a57f81db6c417ff8e1542b066
somlosocram-star <somlosocram@gmail.com>
2026-07-25 12:22:40 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'onix-v0.29.1';
+const CACHE = 'onix-v0.30.0';
 const ASSETS = ['./', './index.html', './manifest.webmanifest',
   './icon-192.png', './icon-512.png', './favicon-32.png', './apple-touch-icon.png', './icon-maskable-512.png'];
 self.addEventListener('install', e => {
