a5fc62f2399d2cd98636449916b2a6c549bb6c71
somlosocram-star <somlosocram@gmail.com>
2026-07-31 16:23:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'onix-v0.47.4';
+const CACHE = 'onix-v0.47.5';
 // La música NO va aquí: los archivos grandes hacen fallar la instalación del
 // service worker. Se cachean solos la primera vez que suenan.
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './sello.png',
